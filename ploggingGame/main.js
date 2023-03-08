d7e1f5a2d7a79fd87843b271199321c8b85c2e6c
Lee Juhyun <heyjace777@gmail.com>
2023-03-08 19:41:03 +0900

    add game-timer function

--- ploggingGame/main.js
+++ ploggingGame/main.js
@@ -4,14 +4,19 @@ const gamePlayBtn = document.querySelector(".game_play_btn");
 const gameTimer = document.querySelector(".game_timer");
 const gameScore = document.querySelector(".game_score");
 
+const popUp = document.querySelector(".pop_up");
+const popUpMsg = document.querySelector(".pop_up_message");
+const popUpReplayBtn = document.querySelector(".pop_up_replay_btn");
+
 const TURTLE_WIDTH = 80;
 const TURTLE_HEIGHT = 55;
 const TURTLE_COUNT = 10;
 const TRASH_COUNT = 5;
+const GAME_TIMER = 10;
 
 let started = false;
 let score = 0;
-let timer = undefined;
+let timer;
 
 gamePlayBtn.addEventListener("click", () => {
   if (started == true) {
@@ -26,9 +31,24 @@ gamePlayBtn.addEventListener("click", () => {
 function startGame() {
   initGame();
   showStopBtn();
+  showTimerAndScore();
+  startGameTimer();
 }
 
-function stopGame() {}
+function stopGame() {
+  stopGameTimer();
+  hidePlayButton();
+  showPopUpWithText("REPLAY");
+}
+
+function hidePlayButton() {
+  gamePlayBtn.style.visibility = "hidden";
+}
+
+function showPopUpWithText(text) {
+  popUp.classList.remove("pop_up_hidden");
+  popUpMsg.innerText = text;
+}
 
 function showStopBtn() {
   const icon = document.querySelector(".play_icon");
@@ -36,7 +56,37 @@ function showStopBtn() {
   icon.classList.remove("fa-play");
 }
 
+function showTimerAndScore() {
+  gameTimer.style.visibility = "visible";
+  gameScore.style.visibility = "visible";
+}
+
+function startGameTimer() {
+  let timerSec = GAME_TIMER;
+  updateGameTimer(timerSec);
+  timer = setInterval(() => {
+    if (timerSec <= 0) {
+      stopGameTimer();
+      return;
+    }
+    updateGameTimer(--timerSec);
+  }, 1000);
+}
+
+function stopGameTimer() {
+  clearInterval(timer);
+  gameTimer.innerText = `0 : 0`;
+}
+
+function updateGameTimer(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerText = `${minutes} : ${seconds}`;
+}
+
 function initGame() {
+  field.innerHTML = "";
+  gameScore.innerText = TRASH_COUNT;
   additem("trash", TRASH_COUNT, "./img/trash.png");
   additem("turtle", TURTLE_COUNT, "./img/turtle.png");
 }
