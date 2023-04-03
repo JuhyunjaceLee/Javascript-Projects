4833b99c33b4dc0f0e8e0fae45da8f2bd67e99e4
unknown <heyjace777@gamil.com>
2023-04-03 14:44:35 +0900

    modified main-home-btn bug

--- portfolioPage/main.js
+++ portfolioPage/main.js
@@ -19,8 +19,6 @@ navList.addEventListener("click", (e) => {
 
 function homeMainBoxClickHandler(e) {
   let elem = e.target;
-  let target = elem.dataset.page;
-  console.log(target);
   while (!elem.classList.contains("box")) {
     elem = elem.parentNode;
     if (elem.nodeName === "BODY") {
@@ -28,7 +26,7 @@ function homeMainBoxClickHandler(e) {
       return;
     }
   }
-  scrollIntoView(target);
+  scrollIntoView(elem.dataset.page);
 }
 
 homeMain.addEventListener("click", homeMainBoxClickHandler);
@@ -50,98 +48,6 @@ navTitle.addEventListener("click", (e) => {
   const target = e.target.dataset.link;
   scrollIntoView(target);
 });
-
-// for (let i = 0; i < mainBoxLength; i++) {
-//   const box = mainBox[i];
-//   box.addEventListener("mouseenter", () => {
-//     box.style.backgroundColor = "var(--color-orange)";
-//   });
-//   box.addEventListener("mouseleave", () => {
-//     box.style.backgroundColor = "var(--color-darkgray)";
-//     myName.style.color = "var(--color-orange)";
-//   });
-// }
-// mainBox[0].addEventListener("mouseenter", () => {
-//   myName.style.color = "var(--color-black)";
-//   mainBox[0].innerText = "ABOUT ME";
-//   mainBox[0].style.fontSize = "3.5rem";
-//   mainBox[0].style.padding = "60px";
-// });
-// mainBox[0].addEventListener("mouseleave", () => {
-//   mainBox[0].innerHTML = `
-//   <div>
-//     <p>안녕하세요!</p>
-//     <p><span class="my-name">이주현</span>입니다.😊</p>
-//     <p style="font-size: 0.8rem">"Front-End Developer"</p>
-//   </div>
-//   <i class="fa-solid fa-circle-arrow-right"></i>
-//   `;
-//   mainBox[0].style.fontSize = "1.5rem";
-// });
-// mainBox[1].addEventListener("mouseenter", () => {
-//   const htmlStr = `
-//   <div class="project-subtitle">
-//     <p>Let's See</p>
-//     <p>My All Projects</p>
-//   </div>
-//   <div class="project-cover" data-page="projectPage"></div>
-//   `;
-//   mainBox[1].innerHTML = htmlStr;
-// });
-// mainBox[1].addEventListener("mouseleave", () => {
-//   mainBox[1].innerHTML = `
-//   <p>PROJECTS</p>
-//   <i class="fa-solid fa-circle-arrow-right"></i>
-//   `;
-// });
-// mainBox[2].addEventListener("mouseenter", () => {
-//   mainBox[2].innerHTML = `
-//   <div>
-//     <div class="skills_icon_wrap">
-//       <i class="fa-brands fa-html5"></i>
-//       <i class="fa-brands fa-css3-alt"></i>
-//     </div>
-//     <div class="skills_icon_wrap">
-//       <i class="fa-brands fa-js"></i>
-//       <i class="fa-brands fa-react"></i>
-//       <i class="fa-brands fa-vuejs"></i>
-//     </div>
-//   </div>
-//   <div class="skill-cover" data-page="skillPage"></div>
-// `;
-// });
-// mainBox[2].addEventListener("mouseleave", () => {
-//   mainBox[2].innerHTML = `
-//   <p>SKILLS</p>
-//   <i class="fa-solid fa-circle-arrow-right"></i>
-//   `;
-// });
-// mainBox[3].addEventListener("mouseenter", () => {
-//   mainBox[3].innerHTML = `
-//   <div class="contact_icon_wrap">
-//     <i class="fa-solid fa-phone"></i>
-//     <i class="fa-solid fa-at"></i>
-//     <i class="fa-brands fa-github"></i>
-//     <i class="fa-regular fa-clipboard"></i>
-//   </div>
-//   <div class="contact-cover" data-page="contactPage"></div>
-
-// `;
-// });
-// mainBox[3].addEventListener("mouseleave", () => {
-//   mainBox[3].innerHTML = `
-//   <p>CONTACT ME</p>
-//   <i class="fa-solid fa-circle-arrow-right"></i>
-//   `;
-// });
-// for (let i = 0; i < mainBoxLength; i++) {
-//   const box = mainBox[i];
-//   box.classList.remove(".active");
-//   box.addEventListener("click", (e) => {
-//     const target = e.target;
-//     target.classList.add("active");
-//   });
-// }
 
 function scrollIntoView(el) {
   const element = document.getElementById(el);
